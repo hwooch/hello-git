adffd6fe11efec1870c999e4f444eaa0e0a271b4
Woochang Hong <hwoochhh@nvaer.com>
2023-10-02 09:55:32 +0000

    C4: modify hello.java

--- Hello.java
+++ Hello.java
@@ -4,5 +4,6 @@ public class Hello{
 	public static void main(String[] args){
 		System.out.println("hello, world from GIT!");
 		System.out.println("Changes after First commit!");
+		System.out.println("changes after Pushing to remote!");
 	}
 }
